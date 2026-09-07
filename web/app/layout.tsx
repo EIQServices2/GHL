0506cd62d64d15bba0e11692b58dea13ea992eed
Vishal@EIQS2.2 <vishal.sahu@eiqdigital.com>
2026-09-07 21:02:18 +0530

    [h5] EIQ-GHL1: add eiqdigital.com favicon + icons metadata

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -53,6 +53,10 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
