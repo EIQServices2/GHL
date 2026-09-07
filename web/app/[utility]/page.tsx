1b64cd11064f7f03d7aef0cc9bb81a238b4d6c8b
Vishal@EIQS2.2 <vishal.sahu@eiqdigital.com>
2026-09-07 21:41:22 +0530

    [h5] EIQ-GHL1: utility links section on all pages, distinct surface bg

--- web/app/[utility]/page.tsx
+++ web/app/[utility]/page.tsx
@@ -1,12 +1,13 @@
 import { notFound } from "next/navigation";
 import type { Metadata } from "next";
-import { getPageData, getUtilitySlugs } from "@/lib/data";
+import { getPageData, getUtilitySlugs, getUtilities } from "@/lib/data";
 import { t } from "@/lib/i18n";
 import { HeaderSection } from "@/sections/HeaderSection";
 import { HeroSection } from "@/sections/HeroSection";
 import { RateComparisonSection } from "@/sections/RateComparisonSection";
 import { WhyItMattersSection } from "@/sections/WhyItMattersSection";
 import { TrialFormSection } from "@/sections/TrialFormSection";
+import { UtilityLinksSection } from "@/sections/UtilityLinksSection";
 import { FooterSection } from "@/sections/FooterSection";
 
 export function generateStaticParams(): { utility: string }[] {
@@ -53,6 +54,7 @@ export default async function UtilityPage({
       <RateComparisonSection rateChange={data.rateChange} />
       <WhyItMattersSection valueProps={data.valueProps} />
       <TrialFormSection cta={data.cta} />
+      <UtilityLinksSection utilities={getUtilities()} />
       <FooterSection />
     </main>
   );
